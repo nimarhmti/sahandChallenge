31777ceb7a9f00a80fc96c035243af1efb5a0e13
nimarhmti <nimarahmati2000@gmail.com>
2023-08-08 14:24:04 +0330

    done

--- lib/data.ts
+++ lib/data.ts
@@ -3,21 +3,21 @@ import { PersonModel } from "@/Services/Persons/Person.interface";
 export const persons: PersonModel[] = [
   {
     id: 1,
-    userId: 4490495781,
+    userId: "4490495781",
     fullName: "نیمارحمتی",
     education: "لیسانس",
     status: "فعال",
   },
   {
     id: 2,
-    userId: 4490493481,
+    userId: "4490493481",
     fullName: "سینا",
     education: "دیپلم",
     status: "غیر فعال",
   },
   {
     id: 3,
-    userId: 4490467781,
+    userId: "4490467781",
     fullName: "سعید",
     education: "فوق لیسانس",
     status: "فعال",
